0fb4d5ba6d7492a69055139f6fa506fcd5a45461
shakao <34112083+shakao@users.noreply.github.com>
2021-10-13 15:41:37 -0700

    0.0.7

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.6.24/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.6","target":"arcade","targetVersion":"1.6.24","repo":"shakao/skillmap-game-of-life"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.6.24/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.7","target":"arcade","targetVersion":"1.6.24","repo":"shakao/skillmap-game-of-life"}
 // total=1145461 new=5.53% cached=88.83% other=5.64%
 (function (ectx) {
 'use strict';
@@ -387,7 +387,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P34337(s) {
+function _main___P34652(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -737,13 +737,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P34337.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P34337.continuations = [  ]
+_main___P34652.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P34652.continuations = [  ]
 
-function _main___P34337_mk(s) {
+function _main___P34652_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P34337, depth: s.depth + 1,
+        parent: s, fn: _main___P34652, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -38793,5 +38793,5 @@ const sprites_BaseSpriteSayRenderer__C19297_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P34337
+return _main___P34652
 })
